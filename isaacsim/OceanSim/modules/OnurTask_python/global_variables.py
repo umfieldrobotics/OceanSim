441077057ccebf99f18bcb276bd06056d857f988
Haoyu <haoyu@jingyuso-FRoG-Desktop.engin.umich.edu>
2025-03-14 16:05:25 -0400

    added IMU sensor Implementation

---
+++ isaacsim/OceanSim/modules/OnurTask_python/global_variables.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2022-2025, NVIDIA CORPORATION. All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto. Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+#
+
+EXTENSION_TITLE = "Onur Log"
+
+EXTENSION_DESCRIPTION = "This is the task for Onur purpose"
+
+
+EXTENSION_LINK = "https://umfieldrobotics.github.io/OceanSim/"
