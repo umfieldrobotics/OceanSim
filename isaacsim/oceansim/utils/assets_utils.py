6459b7ed701184080b496c246d6b5206f65987d7
Seth Isaacson <sethisaacson99@gmail.com>
2025-06-09 17:42:49 -0400

    Update assets_utils.py
    Set a more general default location of oceansim assets.

--- isaacsim/oceansim/utils/assets_utils.py
+++ isaacsim/oceansim/utils/assets_utils.py
@@ -1,3 +1,9 @@
+import os
+
+OCEANSIM_ROOT = os.path.normpath(
+    os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir)
+)
+ASSETS_PATH = os.path.join(OCEANSIM_ROOT, 'Assets')
+
 def get_oceansim_assets_path() -> str:
-    # return "/home/haoyu/Desktop/OceanSim_assets"
-    return "/home/haoyu-ma/Desktop/OceanSim_assets"
+    return ASSETS_PATH
